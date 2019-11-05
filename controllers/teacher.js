5cb859d35e32c43a5c5c4c3f336ee04e4ec97d42
shwetagurnani <shwetaagra28@gmail.com>
2019-11-05 19:58:10 +0530

    chamges in teacher routes

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -4,13 +4,11 @@ const db=require('../util/database');
 exports.getviewAtt=(req,res,next)=>{
 res.render('viewatt',{
     pageTitle:'shweta',
-    // StudentCount:52
+   
 });
 }
 
 exports.postviewAtt=(req,res,next)=>{
-    // res.send('hey');
-    // console.log('huff');
     const att=req.body.att;
     res.send(att);
     };
