55f8ef84e9ad35a8339af5aa396b364c2de487ed
shweta gurnani <43384092+shwetagurnani@users.noreply.github.com>
2019-11-05 09:30:02 +0530

    Update teacher.js

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -1,8 +1,12 @@
-exports.getFront=(req,res,next)=>{
+exports.viewAtt=(req,res,next)=>{
 
 };
 
 
-exports.postFront=(req,res,next)=>{
+exports.getAtt=(req,res,next)=>{
+
+};
+
+exports.postAtt=(req,res,next)=>{
 
 };
